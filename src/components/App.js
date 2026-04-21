af9d9e1a8a9e4e0102f3b67d70394939902643b7
Atharva Lonikar <126399397+atharvalonikar95@users.noreply.github.com>
2026-04-21 14:44:34 +0530

    Add state management and child component interaction

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,19 @@
 
 import React from "react";
 import './../styles/App.css';
-
+import Child from './comp/Child'
 const App = () => {
+    const [selectedOption,setSelectedOption]=React.useState()
+    const handleOption=(optionNo:any)=>{
+       setSelectedOption(optionNo)
+        
+    }
+
   return (
     <div>
-        {/* Do not remove the main div */}
+        <Child optionNo={1} handleOption={handleOption} />
+        <Child optionNo={2} handleOption={handleOption} />
+        <p>selected : option {selectedOption}</p>
     </div>
   )
 }
